33e094c4a91c92589c29e1ce8561e6c9fb60918c
chulc1114 <chulc1114@thundersoft.com>
2020-11-24 17:15:41 +0800

    添加比较通用的获取finalX的方法

--- app/src/main/java/com/clc/psv/PageScrollView.java
+++ app/src/main/java/com/clc/psv/PageScrollView.java
@@ -8,8 +8,9 @@ import android.widget.Scroller;
 
 public class PageScrollView extends HorizontalScrollView {
     public static final int PAGE_WIDTH = 1000;
-    public static final int DISTANCE_LIMIT = 500;
+    public static final int DISTANCE_LIMIT = 300;
     private Scroller mScroller;
+    private int mDownX;
     private PageChangedListener mPageChangedListener;
 
     public void setPageChangedListener(PageChangedListener pageChangedListener) {
@@ -33,10 +34,17 @@ public class PageScrollView extends HorizontalScrollView {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        /**
+         * 记录mDownX，以判断滑动方向
+         * 如果滑动临界是页宽的一半，不用记录mDownX
+         */
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            mDownX = getScrollX();
+        }
         if (event.getAction() == MotionEvent.ACTION_UP) {
-            int scrollX = getScrollX();
-            int finalX = getFinalX();
-            mScroller.startScroll(scrollX, 0, finalX - scrollX, 0);
+            int currX = getScrollX();
+            int finalX = findFinalX(currX);
+            mScroller.startScroll(currX, 0, finalX - currX, 0);
             invalidate();
             if (mPageChangedListener != null) {
                 mPageChangedListener.onPageChanged(finalX / PAGE_WIDTH + 1);
@@ -45,12 +53,34 @@ public class PageScrollView extends HorizontalScrollView {
         return super.onTouchEvent(event);
     }
 
-    private int getFinalX() {
-        int num = getScrollX() / DISTANCE_LIMIT;
-        if ((num & 1) == 1) {
-            return DISTANCE_LIMIT * (num + 1);
+    /**
+     * 获取scroll终点（如果滑动临界是页宽的一半）
+     */
+    /*private int findFinalX(int currX) {
+        int multiple = currX / DISTANCE_LIMIT;
+        if ((multiple & 1) == 1) {
+            return DISTANCE_LIMIT * (multiple + 1);
         } else {
-            return DISTANCE_LIMIT * num;
+            return DISTANCE_LIMIT * multiple;
+        }
+    }*/
+
+    /**
+     * 获取scroll终点（较为通用）
+     */
+    private int findFinalX(int currX) {
+        int remainder = currX % PAGE_WIDTH;
+        int multiple = currX / PAGE_WIDTH;
+        if (remainder < DISTANCE_LIMIT) {
+            return PAGE_WIDTH * multiple;
+        } else if (remainder > PAGE_WIDTH - DISTANCE_LIMIT) {
+            return PAGE_WIDTH * (multiple + 1);
+        } else {
+            if (currX > mDownX) {
+                return PAGE_WIDTH * (multiple + 1);
+            } else {
+                return PAGE_WIDTH * (multiple);
+            }
         }
     }
 
